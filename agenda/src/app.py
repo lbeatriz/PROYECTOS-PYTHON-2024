83187e5e8165cc3e56848385766e0cf8b0f24d0f
theundertaker0 <amadeusdigital@gmail.com>
2024-03-18 17:03:31 -0600

    CRUD Listo

--- agenda/src/app.py
+++ agenda/src/app.py
@@ -1,4 +1,5 @@
 from flask import Flask,render_template,request,redirect,url_for,session
+from datetime import datetime
 import config
 from flask_mysqldb import MySQL
 
@@ -31,9 +32,54 @@ def login():
     else:
         return render_template('login.html', message="Email o contraseña incorrectos")
 
-@app.route("/tasks")
+@app.route("/tasks",methods=['GET'])
 def tasks ():
-    return render_template('tasks.html')
+  
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM tasks")
+    tasks = cur.fetchall()
+    cur.close()
+    return render_template('tasks.html', tasks = tasks)
+
+
+@app.route('/add_task', methods=['POST'])
+def add_task():
+    if request.method == 'POST':
+        nombre = request.form['nombre']
+        descripcion = request.form['descripcion']
+        cur = mysql.connection.cursor()
+        # Obtener la fecha y hora actual
+        fecha_actual = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        cur.execute("INSERT INTO tasks (nombre, descripcion,email,fecha) VALUES (%s, %s, %s, %s)", (nombre, descripcion,session['email'],fecha_actual))
+        mysql.connection.commit()
+        cur.close()
+        return redirect(url_for('tasks'))
+    
+@app.route('/edit_task/<int:id>', methods=['GET', 'POST'])
+def edit_task(id):
+    cur = mysql.connection.cursor()
+    if request.method == 'POST':
+        nombre = request.form['nombre']
+        descripcion = request.form['descripcion']
+        cur.execute("UPDATE tasks SET nombre = %s, descripcion = %s WHERE id = %s", (nombre, descripcion, id))
+        mysql.connection.commit()
+        cur.close()
+        return redirect(url_for('tasks'))
+    else:
+        cur.execute("SELECT * FROM tasks WHERE id = %s", (id))
+        task = cur.fetchone()
+        cur.close()
+        return render_template('edit.html', task=task)
+
+@app.route('/delete_task', methods=['POST'])
+def delete_task():
+    cur = mysql.connection.cursor()
+    id = request.form['task_id']
+    print("Valor de id:", id)
+    cur.execute("DELETE FROM tasks WHERE id = %s", (id))
+    mysql.connection.commit()
+    cur.close()
+    return redirect(url_for('tasks'))
 
 if __name__ =='__main__':
     app.run(debug=True)
